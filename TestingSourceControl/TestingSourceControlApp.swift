76ca08ab75c93ececb6f3954650c6f524fd5d764
Tech-Mex6 <okekemeekam@gmail.com>
2024-05-17 22:00:48 -0500

    Initial Commit

---
+++ TestingSourceControl/TestingSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSourceControlApp.swift
+//  TestingSourceControl
+//
+//  Created by meekam okeke on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
