4d67c12274add3c0e925160de987ed69482646a6
Tech-Mex6 <okekemeekam@gmail.com>
2024-05-17 23:00:47 -0500

    HomeView added

---
+++ TestingSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TestingSourceControl
+//
+//  Created by meekam okeke on 5/17/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
