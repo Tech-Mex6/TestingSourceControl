2c97494547c63d3e5b4b01938c8db71a330ac64a
Tech-Mex6 <okekemeekam@gmail.com>
2024-05-18 15:16:42 -0500

    [Feature] Button title updated to Tap me Again

--- TestingSourceControl/ContentView.swift
+++ TestingSourceControl/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful thinking!")
             
-            Button("Tap Me!") {
+            Button("Tap Me Again!") {
                 
             }
             Rectangle()
